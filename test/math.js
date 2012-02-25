f5ba4b646436cd8688d0fa44478cc5abcc0eed0a
ANDO Takahiro <takahiro.ando@gmail.com>
2012-02-25 18:56:55 +0900

    migrate from jasmine to mocha

--- test/math.js
+++ test/math.js
@@ -1,41 +1,47 @@
-(function() {
-  var EPSILON = 0.0000001;
+var expect = require('expect.js'),
+    fs = require('fs'),
+    vm = require('vm'),
+    EPSILON = 0.0000001;
 
-  describe('math.plane', function() {
-    it('calcs plane 4 dimeension vector', function() {
-      expect(Math.abs(
-        math.plane(
-          math.v3(1, 1, 1), // 平面原点
-          math.v3(0, 1, 0) // 法線
-        ).dot(
-          math.v4(-20.123, 1, 20, 1) // 任意点(平面上)
-        )) < EPSILON).toBe(true);
+global.window = global;
+window.self = global;
+vm.runInThisContext(fs.readFileSync('public/vendor/three.js/Three.js'));
+vm.runInThisContext(fs.readFileSync('public/js/math.js'));
 
-      expect(Math.abs(
-        math.plane(
-          math.v3(1, 1, 1), // 平面原点
-          math.v3(0, 1, 0) // 法線
-        ).dot(
-          math.v4(-20.123, 2, 20, 1) // 任意点(平面「外」)
-        )) < EPSILON).toBe(false);
-    });
+describe('math.plane', function() {
+  it('calcs plane 4 dimeension vector', function() {
+    expect(Math.abs(
+      math.plane(
+        math.v3(1, 1, 1), // 平面原点
+        math.v3(0, 1, 0) // 法線
+      ).dot(
+        math.v4(-20.123, 1, 20, 1) // 任意点(平面上)
+      )) < EPSILON).to.be(true);
+
+    expect(Math.abs(
+      math.plane(
+        math.v3(1, 1, 1), // 平面原点
+        math.v3(0, 1, 0) // 法線
+      ).dot(
+        math.v4(-20.123, 2, 20, 1) // 任意点(平面「外」)
+      )) < EPSILON).to.be(false);
+  });
+});
+
+describe('math.intersectRayAndPlane', function() {
+  it('returns intersection position', function() {
+    var actual = math.intersectRayAndPlane(
+      math.v3(1, -1, 1), math.v3(0, 1, 0),
+      math.plane(math.v3(0, 0.5, 0), math.v3(0, 1, 0)));
+
+    expect(actual.distanceTo(math.v3(1, 0.5, 1)) < EPSILON).to.be(true);
   });
 
-  describe('math.intersectRayAndPlane', function() {
-    it('returns intersection position', function() {
-      var actual = math.intersectRayAndPlane(
-        math.v3(1, -1, 1), math.v3(0, 1, 0),
-        math.plane(math.v3(0, 0.5, 0), math.v3(0, 1, 0)));
+  it('returns null if intersection is not found', function() {
+    var actual = math.intersectRayAndPlane(
+      math.v3(1, -1, 1), math.v3(0, -1, 0),
+      math.plane(math.v3(0, 0.5, 0), math.v3(0, 1, 0)));
 
-      expect(actual.distanceTo(math.v3(1, 0.5, 1)) < EPSILON).toBe(true);
-    });
-
-    it('returns null if intersection is not found', function() {
-      var actual = math.intersectRayAndPlane(
-        math.v3(1, -1, 1), math.v3(0, -1, 0),
-        math.plane(math.v3(0, 0.5, 0), math.v3(0, 1, 0)));
-
-      expect(actual).toBe(null);
-    });
+    expect(actual).to.be(null);
   });
-})();
+});
